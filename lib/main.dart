2aee4af6df23d55a4751f2016c7fd0d89ae527f6
houki0521 <130425531+houki0521@users.noreply.github.com>
2024-10-22 16:12:26 +0900

    最初のコミット

---
+++ lib/main.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 29, 161, 242),
+          title: const Text(
+            'こんぶ @ Flutter大学',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+              TweetTile(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class TweetTile extends StatelessWidget {
+  const TweetTile({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        // Column を Row でラップ
+        children: [
+          // ここにユーザーアイコンを追加する
+          CircleAvatar(
+            backgroundImage: NetworkImage('http:'),
+          ),
+          SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text('こんぶ @ Flutter大学'),
+                  SizedBox(width: 8),
+                  Text('2022/05/05'),
+                ],
+              ),
+              SizedBox(height: 4),
+              Text('最高でした。'),
+              IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.favorite_border),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
