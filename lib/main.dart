683496c855ad08e504c0e261f3ae162052cd2af6
Gandamu108 <105183098+Gandamu108@users.noreply.github.com>
2024-11-06 14:49:08 +0900

    コミットメッセージ

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'Store_Screens.dart';
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -10,7 +9,6 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,39 +22,136 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
   final String title;
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: RichText(
-          text: TextSpan(
-            children: <TextSpan>[
-              TextSpan(
-                text: 'ホーム(未完成)',
-                style: TextStyle(color: Colors.blue),
-                recognizer: TapGestureRecognizer()
-                  ..onTap = () {
-                    // ここにタップ時のアクションを追加します
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => Store_ScreensPage()),
-                    );
-                  },
-                ),
-              ]
-            ),
-          ),
+      appBar: AppBar(
+        title: Text('ホーム画面'),
       ),
+      body: Container(
+        child: ListView(
+          children: <Widget>[
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Store_ScreensPage()),
+                );
+              },
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+            Card(
+              child: ListTile(
+              title: Text('タイトル'),
+              subtitle: Text('サブタイトル'),
+            ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
