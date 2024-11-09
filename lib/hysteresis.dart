67908ee15bc69ec8023fed63705f1fad8a6b5908
houki0521 <130425531+houki0521@users.noreply.github.com>
2024-11-09 13:14:26 +0900

    コミットメッセージ

---
+++ lib/hysteresis.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Hysteresis extends StatefulWidget {
+  const Hysteresis({super.key});
+
+  @override
+  State<Hysteresis> createState() => _HysteresisState();
+}
+
+class _HysteresisState extends State<Hysteresis> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        
+      ),
+    );
+  }
+}
